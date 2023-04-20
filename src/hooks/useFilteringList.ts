451ae5bfecfb6dfb9ebfc17f1e6f565ffffb34f6
nlom0218 <nlom0218@naver.com>
2023-04-20 23:32:03 +0900

    refactor: useFilteringList 타입 선언

--- src/hooks/useFilteringList.ts
+++ src/hooks/useFilteringList.ts
@@ -1,16 +1,19 @@
 import { useState } from 'react';
 
-const useFilteringList = <T, U extends (arg: T) => T, K extends Record<keyof K, U>>(
-  list: T,
-  filteringFn: Record<keyof K, U>,
-): [T, (subject: keyof K, filteringFn: (arg: T) => T) => void] => {
+type FilterFnCallbackFn<T> = (arg: T) => T;
+type FilterFn<K, U> = Record<keyof K, U>;
+
+const useFilteringList = <List, CallBackFn extends FilterFnCallbackFn<List>, K extends FilterFn<K, CallBackFn>>(
+  list: List,
+  filteringFn: FilterFn<K, CallBackFn>,
+): [List, (subject: keyof K, filteringFn: FilterFnCallbackFn<List>) => void] => {
   const [_filteringFn, setFilteringFn] = useState(filteringFn);
 
   const filteringPipe = () => {
-    return (Object.values(_filteringFn) as U[]).reduce((_list, fn) => fn(_list), list);
+    return (Object.values(_filteringFn) as CallBackFn[]).reduce((_list, fn) => fn(_list), list);
   };
 
-  const changeFilteringFn = (subject: keyof K, filteringFn: (arg: T) => T) => {
+  const changeFilteringFn = (subject: keyof K, filteringFn: FilterFnCallbackFn<List>) => {
     setFilteringFn((prev) => {
       return {
         ...prev,
